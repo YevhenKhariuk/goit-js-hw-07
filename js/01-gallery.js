f4fff91a91802d1343817420ad8b41b6f15702b4
YevhenKhariuk <777genya2009@gmail.com>
2023-05-30 19:01:39 +0300

    DZ v1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const addImgMark = createGallery(galleryItems);
+gallery.insertAdjacentHTML("beforeend", addImgMark);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href=${original}>
+      <img class="gallery__image" src=${preview} data-source=${original} alt=${description}/></a>
+      </li>`;
+    })
+    .join("");
+}
+
+gallery.onclick = (cl) => {
+  cl.preventDefault();
+  if (cl.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const openImg = basicLightbox.create(
+    `<img width="800" height="600" src="${cl.target.dataset.source}">`,
+    {
+      closable: true,
+      onShow: () => {
+        document.body.addEventListener("keydown", pressEsc);
+      },
+      onClose: () => {
+        document.body.removeEventListener("keydown", pressEsc);
+      },
+    }
+  );
+
+  function pressEsc(evt) {
+    if (evt.key === "Escape") {
+      openImg.close(evt);
+    }
+  }
+
+  openImg.show();
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const addImgMark = createGallery(galleryItems);
+gallery.insertAdjacentHTML("beforeend", addImgMark);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__item" href=${original}>
+      <img class="gallery__image" src=${preview} alt=${description}/>
+      </a>
+      </li>`;
+    })
+    .join("");
+}
+
+const addGallarySimpleLightBox = new SimpleLightbox(".gallery a", {
+  overlayOpacity: 0.9,
+  captionsData: "alt",
+  captionDelay: 250,
+  animationSpeed: 500,
+});
